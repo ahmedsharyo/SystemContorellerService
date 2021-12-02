35e412f6e0d98ce5203f6d193cdb76185fe8db25
ahmedsharyo <ahmedsharyo@gmail.com>
2021-12-02 12:20:52 +0200

    refactor

--- main.go
+++ main.go
@@ -5,19 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// var err error
-
 func main() {
-
-	// Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
-
-	// if err != nil {
-	// 	fmt.Println("Status:", err)
-	// }
-
-	// defer Config.DB.Close()
-
-	// Config.DB.AutoMigrate(&Models.User{})
 
 	server := gin.New()
 
